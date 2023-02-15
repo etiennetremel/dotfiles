77a3c558df193bc473c49506870d3a23c657396f
Etienne Tremel <995474+etiennetremel@users.noreply.github.com>
2023-02-15 21:44:48 +0100

    fix: disable whitespace from formatter in favor of better whitespace

--- nvim/lua/setup/formatter.lua
+++ nvim/lua/setup/formatter.lua
@@ -48,11 +48,11 @@ require("formatter").setup {
 
     -- Use the special "*" filetype for defining formatter configurations on
     -- any filetype
-    ["*"] = {
-      -- "formatter.filetypes.any" defines default configurations for any
-      -- filetype
-      require("formatter.filetypes.any").remove_trailing_whitespace,
-    },
+    -- ["*"] = {
+    --   -- "formatter.filetypes.any" defines default configurations for any
+    --   -- filetype
+    --   require("formatter.filetypes.any").remove_trailing_whitespace,
+    -- },
   },
 }
 
